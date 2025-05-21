b692b93170c02ea92d7a6b84ae195d7e751f8c2f
Santidonatoo <santinodonato1@gmail.com>
2025-05-21 16:48:13 -0300

    Primer commit

---
+++ turnos/src/main/java/oo2/grupo5/turnos/TurnosApplication.java
@@ -0,0 +1,13 @@
+package oo2.grupo5.turnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosApplication.class, args);
+	}
+
+}
---
+++ turnos/src/test/java/oo2/grupo5/turnos/TurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package oo2.grupo5.turnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
