1fba78fce38397b5ca779372b325f6ee977f540a
mareale <echavarria.martin@hotmail.com>
2025-06-12 12:47:16 -0300

    controller rest servicio

---
+++ turnos/src/main/java/oo2/grupo5/turnos/dtos/responses/ServicioApiResponseDTO.java
@@ -0,0 +1,10 @@
+package oo2.grupo5.turnos.dtos.responses;
+
+public record ServicioApiResponseDTO(
+		Integer id,
+	    String nombre,
+	    Integer duracion,
+	    boolean requiereEmpleado,
+	    String ubicacion
+	    
+	    ){}
